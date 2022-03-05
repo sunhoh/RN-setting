f1bb6dc6b576bba27d291c5c9eb26798e9266929
sunhoh <dltjsgho94@gmail.com>
2022-03-05 22:03:38 +0900

    first commit

---
+++ src/navigations/RootStack.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+const RootStack = () => {
+    <Stack.Navigator>
+      <Stack.Screen
+      name=''
+      component=''
+      options={{ }}
+    />
+    </Stack.Navigator>
+}
